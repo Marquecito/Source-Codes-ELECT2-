56789d86075fff502f8644691599e9615bed6592
Marc <89451760+Marquecito@users.noreply.github.com>
2023-03-29 21:03:07 +0800

    Add files via upload

---
+++ Midterm #36.php
@@ -0,0 +1,45 @@
+<?php
+abstract class Arithmetic {
+
+    abstract function add();
+
+    abstract function subtract();
+
+    abstract function multiply();
+
+    abstract function divide();
+
+}
+
+class Calculator extends Arithmetic {
+
+    public $x;
+    public $y;
+
+    /**
+     * @param $x
+     * @param $y
+     */
+    public function __construct($x, $y) {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    function add() {
+        return $this->x + $this->y;
+    }
+
+    function subtract() {
+        return $this->x - $this->y;
+    }
+
+    function multiply() {
+        return $this->x * $this->y;
+    }
+
+    function divide() {
+        return $this->x / $this->y;
+    }
+
+
+}
