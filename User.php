b0fafd9f5abeb42e0faeef7a5b8a7ed29f8fba7b
Marc <89451760+Marquecito@users.noreply.github.com>
2023-03-29 21:01:03 +0800

    Update User.php

--- User.php
+++ User.php
@@ -1,16 +1,26 @@
 <?php
-class User {
+
+abstract class Calculations {
+
+    abstract function getTotalSpending();
+    abstract function getRemainingBalance();
+
+}
+
+class User extends Calculations {
 
     protected string $name;
     protected float $spendingLimit;
+    protected array $products;
 
     /**
      * @param string $name
      * @param float $spendingLimit
      */
-    public function __construct(string $name, float $spendingLimit) {
+    public function __construct(string $name, float $spendingLimit, array $products) {
         $this->name = $name;
         $this->spendingLimit = $spendingLimit;
+        $this->products = $products;
     }
 
     /**
@@ -32,6 +42,32 @@ class User {
      */
     public function setSpendingLimit(float $spendingLimit): void {
         $this->spendingLimit = $spendingLimit;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProducts(): array {
+        return $this->products;
+    }
+
+    /**
+     * @param array $products
+     */
+    public function setProducts(array $products): void {
+        $this->products = $products;
+    }
+
+    public function getTotalSpending() {
+        $totalPrice = 0;
+        foreach ($this->products as $product) {
+            $totalPrice += $product->getPrice();
+        }
+        return $totalPrice;
+    }
+
+    public function getRemainingBalance() {
+        return $this->spendingLimit - $this->getTotalSpending();
     }
 
 }
