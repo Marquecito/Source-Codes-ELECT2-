4e20ffe505979b46c72ce9e637ffe56fb4873427
Marc <89451760+Marquecito@users.noreply.github.com>
2023-03-29 20:14:06 +0800

    Add files via upload

---
+++ Product.php
@@ -0,0 +1,32 @@
+<?php
+
+class Product {
+
+    protected string $name;
+    protected float $price;
+
+    protected float $weight;
+
+    public function __construct(string $name, float $price, float $weight) {
+        $this->name = $name;
+        $this->price = $price;
+        $this->weight = $weight;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getPrice(): float {
+        return $this->price;
+    }
+
+    public function getWeight(): float {
+        return $this->weight;
+    }
+
+    public function __toString(): string {
+        return "$this->name ($this->weight lb): $" . round($this->price, 2);
+    }
+
+}
